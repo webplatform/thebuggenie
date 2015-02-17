3f838716cfe7dc13f66a59f069840b09af575430
Renoir Boulanger <renoir@w3.org>
2015-02-17 10:36:36 -0500

    update piwik tracking script

--- core/templates/footer.inc.php
+++ core/templates/footer.inc.php
@@ -19,6 +19,23 @@
 	    <li class="steward-opera"><a href="//www.webplatform.org/stewards/opera">Opera</a></li>
 	  </ul>
 	</div>
+	<!-- Piwik -->
+	<script type="text/javascript">
+		var _paq = _paq || [];
+		_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+		_paq.push(["setCookieDomain", "*.webplatform.org"]);
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+			var u="https://stats.webplatform.org/";
+			_paq.push(['setTrackerUrl', u+'piwik.php']);
+			_paq.push(['setSiteId', 1]);
+			var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+			g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+		})();
+	</script>
+	<noscript><p><img src="https://stats.webplatform.org/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+	<!-- End Piwik Code -->
 	<?php if (TBGContext::isDebugMode() && TBGLogging::isEnabled()): ?>
 		<div id="tbg___DEBUGINFO___" style="position: fixed; bottom: 0; left: 0; z-index: 100; display: none; width: 100%;">
 		</div>
